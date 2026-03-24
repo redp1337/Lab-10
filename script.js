869586455510b15b039f7369dc365c141f04364d
redp1337 <mparedes754@gmail.com>
2026-03-24 15:24:12 -0400

    add toString() method that returns a string representation of the product

--- script.js
+++ script.js
@@ -11,5 +11,12 @@ class ProductProperties{
         // returns the total value of product in stock
         return this.price*this.quantity;
     }
+    
+    toString(){
+        // returns string representation of the product
+        // price must be rounded to two decimal places
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`
+
+    }
 }
 
