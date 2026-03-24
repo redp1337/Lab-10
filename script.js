0766129f6e234823010da70e2e287067ce65ddf2
redp1337 <mparedes754@gmail.com>
2026-03-24 14:39:33 -0400

    create base class: ProductProperties

---
+++ script.js
@@ -0,0 +1,9 @@
+//Part 1: setting up classes
+
+class ProductProperties{
+    constructor(name, price, quantity){
+        this.name = name;
+        this.price=price;
+        this.quantity=quantity;
+    }
+}
